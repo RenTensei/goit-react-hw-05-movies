71ecdce209394e2303ce9d8dd9f6b69a4070ddfe
RenTensei <scs.bon7@gmail.com>
2023-05-31 11:19:19 +0300

    add styled

---
+++ src/components/Styled.jsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const StyledItem = styled.li`
+  display: block;
+  padding: 8px;
+  margin-bottom: 12px;
+  max-width: fit-content;
+
+  background-color: #873e23;
+  color: #cdb896;
+  text-decoration: none;
+  border-radius: 8px;
+
+  a {
+    text-decoration: none;
+    color: #cdb896;
+
+    &:hover {
+      color: black;
+    }
+  }
+`;
